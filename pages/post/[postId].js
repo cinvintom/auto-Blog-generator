2d881737acffaaa634fd7bf9cf4e75941249dc22
Cinvin Tom <cinvin@qburst.com>
2023-11-29 10:12:55 +0530

    Completed show post page

--- pages/post/[postId].js
+++ pages/post/[postId].js
@@ -2,6 +2,8 @@ import { getSession, withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { AppLayout } from "../../components/AppLayout";
 import clientPromise from "../../lib/mongodb";
 import { ObjectId } from "mongodb";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHashtag } from "@fortawesome/free-solid-svg-icons";
 
 export default function Post(props) {
   return (<div className="overflow-auto h-full">
@@ -12,6 +14,16 @@ export default function Post(props) {
       <div className="p-4 my-2 border border-stone-200 rounded-md">
         <div className="text-blue-600 text-2xl font-bold">{props.title}</div>
         <div className="mt-2" >{props.metaDescription}</div>
+      </div>
+      <div className="text-sm font-bold mt-6 p-2 bg-stone-200 rounded-sm">
+        keywords
+      </div>
+      <div className="flex flex-wrap  pt-2 gap-1">
+        {props.keywords.split(",").map((keyword, i) => (
+          <div key={i} className="p-2 rounded-full bg-slate-800 text-white">
+            <FontAwesomeIcon icon={faHashtag} />{keyword}
+          </div>
+        ))}
       </div>
       <div className="text-sm font-bold mt-6 p-2 bg-stone-200 rounded-sm">
         Blog post
