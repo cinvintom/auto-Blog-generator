d73f3e7be52b074b06bf45ef1a9fd5d2fa59e338
Cinvin Tom <cinvin@qburst.com>
2023-12-05 11:42:59 +0530

    new feature

--- pages/api/webhooks/stripe.js
+++ pages/api/webhooks/stripe.js
@@ -19,43 +19,48 @@ const handler = async (req, res) => {
     if (req.method === 'POST') {
         let event;
         try {
-            const event = verifyStripe({
+            event = verifyStripe({
                 req,
                 stripe,
                 endpointSecret
             });
         } catch (e) {
-            console.log("ERROR: ", e)
+            console.error("ERROR: ", e);
+            return res.status(400).json({ error: 'Webhook verification failed' });
         }
 
         switch (event.type) {
             case 'payment_intent.succeeded': {
-                const client = await clientPromise
+                const client = await clientPromise;
                 const db = client.db("BlogStandard");
 
                 const paymentIntent = event.data.object;
                 const auth0Id = paymentIntent.metadata.sub;
 
-                const userProfile = await db.collection("users").updateOne({
-                    auth0Id
-                }, {
+                try {
+                    const userProfile = await db.collection("users").updateOne(
+                        { auth0Id },
+                        {
+                            $inc: { availableTokens: 10 },
+                            $setOnInsert: { auth0Id },
+                        },
+                        { upsert: true }
+                    );
 
-                    $inc: {
-                        availableTokens: 10
-                    },
-                    $setOnInsert: {
-                        auth0Id,
-                    },
-
-                }, {
-                    upsert: true,
+                    console.log('User profile updated:', userProfile);
+                } catch (error) {
+                    console.error('Error updating user profile:', error);
+                    return res.status(500).json({ error: 'Internal Server Error' });
                 }
-                );
+                break;
             }
             default:
+                console.log('Webhook Event:', event);
+
                 console.log('UNHANDLED EVENT: ', event.type);
         }
-        res.status(200).json({ recieved: true });
+
+        res.status(200).json({ received: true });
     }
 };
 
